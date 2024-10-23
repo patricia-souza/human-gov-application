f118d40e250454a4a8589bde2f712d439e6322a6
Patricia Aparecida de Souza <souza302418@hotmail.com>
2024-10-23 07:49:58 +0000

    added tests

--- src/tests/test_app.py
+++ src/tests/test_app.py
@@ -2,4 +2,5 @@ import pytest
 from app import home_page
 
 def test_home_page():
+    print('Execucao de testes')
     assert home_page('California') == 'Human Resources Management System - State of California'
